4a7edc3fef5f2525c830966dd5b097280ef864d3
Jandeilson Xavier <jandeilsonxs@gmail.com>
2023-11-08 09:14:05 -0300

    Update Banco_de_dados_Projeto1.sql

--- Banco_de_dados_Projeto1.sql
+++ Banco_de_dados_Projeto1.sql
@@ -1,1 +1,14 @@
 CREATE DATABASE cadastro CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+create table clientes(
+id int not null auto_increment, 
+nome varchar(30) not null,
+nascimento date,
+sexo enum('M','F'),
+peso decimal(5,2),
+altura decimal(3,2),
+nacionalidade varchar(20) default 'Brasil',
+primary key (id)
+)default charset utf8;
+
+use sys;
