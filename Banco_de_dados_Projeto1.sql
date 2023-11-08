b8fd78f26166664464f4ab392ecec88fffb9acd2
Jandeilson Xavier <jandeilsonxs@gmail.com>
2023-11-08 15:52:11 -0300

    Update Banco_de_dados_Projeto1.sql

--- Banco_de_dados_Projeto1.sql
+++ Banco_de_dados_Projeto1.sql
@@ -48,4 +48,14 @@ describe pessoas;
 # ou
 desc pessoas;
 
+create table if not exists cursos(
+nome varchar(30) not null unique,
+descricao text,
+carga int unsigned,
+totaulas int unsigned,
+ano year default '2016'
+) default charset=utf8;
 
+alter table cursos add column idcurso int first;
+# Aciciona uma chave primaria na tabela
+alter table cursos add primary key(idcurso);
