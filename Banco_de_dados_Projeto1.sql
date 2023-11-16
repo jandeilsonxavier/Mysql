3173750a5943849001c58537e47c464e6b3fc28b
Jandeilson Xavier <jandeilsonxs@gmail.com>
2023-11-15 23:42:40 -0300

    Update Banco_de_dados_Projeto1.sql

--- Banco_de_dados_Projeto1.sql
+++ Banco_de_dados_Projeto1.sql
@@ -13,7 +13,11 @@ nacionalidade varchar(20) default 'Brasil',
 primary key (id)
 )default charset utf8;
 
+select * from pessoas;
+
 insert into clientes values
 (default, 'Jhon', '2000-08-30', 'M', '100.3', '1.85', 'EUA'),
 (default, 'Clara', '2004-01-3', 'F', '59', '1.69', 'Brasil'),
 (default, 'João', '1995-12-2', 'M', '80', '1.78', 'Espanha');
+
+update pessoas set nome = 'Ana' where id = '4';
